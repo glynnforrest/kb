8fd2deb407ef4bb5cd6e983b4e6835fa703deefd
Glynn Forrest <glynn@forrest.net>
2026-06-08 00:37:51 -0500

    Create IDs from a filepath

--- internal/xid/xid.go
+++ internal/xid/xid.go
@@ -1,6 +1,9 @@
 package xid
 
 import (
+	"fmt"
+	"strings"
+
 	"math/rand/v2"
 	"path/filepath"
 )
@@ -18,6 +21,21 @@ func New() ID {
 		id[i] = alphabet[rand.IntN(len(alphabet))]
 	}
 	return ID(id)
+}
+
+func FromPath(path string) (ID, error) {
+	pathNoExt := strings.TrimSuffix(path, ".md")
+	if len(pathNoExt) < 9 {
+		return ID(""), fmt.Errorf("path is too short: %s", path)
+	}
+	last9 := pathNoExt[len(pathNoExt)-9:]
+	last8 := strings.ReplaceAll(last9, string(filepath.Separator), "")
+
+	if len(last8) != 8 {
+		return ID(""), fmt.Errorf("parsed ID from path %s should be 8 chars, got: %s", path, last8)
+	}
+
+	return ID(last8), nil
 }
 
 func (i ID) Path() string {
--- internal/xid/xid_test.go
+++ internal/xid/xid_test.go
@@ -11,11 +11,79 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestFromPath(t *testing.T) {
+	for _, tc := range []struct {
+		given string
+		expected string
+		wantErr bool
+	}{
+		{
+			given: "",
+			wantErr: true,
+		},
+		{
+			given: "abcd",
+			wantErr: true,
+		},
+		{
+			given: "abcdefgh.md",
+			wantErr: true,
+		},
+		{
+			given: "ab/cdefgh.md",
+			expected: "abcdefgh",
+		},
+		{
+			given: "/path/to/notes/ab/cdefgh.md",
+			expected: "abcdefgh",
+		},
+		{
+			given: "path/to/notes/ab/cdefgh.md",
+			expected: "abcdefgh",
+		},
+		{
+			given: "path/to/notes/ab/short.md",
+			wantErr: true,
+		},
+	} {
+		t.Run(tc.given, func(t *testing.T) {
+			actual, err := FromPath(tc.given)
+			if tc.wantErr {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+			if tc.expected != string(actual) {
+				t.Fatalf("expected %v, got %v", tc.expected, actual)
+			}
+		})
+	}
+}
+
 func TestPath(t *testing.T) {
-	id := ID("abcdefgh")
-	expected := "ab/cdefgh.md"
-	actual := id.Path()
-	if expected != actual {
-		t.Fatalf("expected %v, got %v", expected, actual)
+	for _, tc := range []struct {
+		given string
+		expected string
+	}{
+		{
+			given: "abcdefgh",
+			expected: "ab/cdefgh.md",
+		},
+		{
+			given: "a1b2c3d4",
+			expected: "a1/b2c3d4.md",
+		},
+	} {
+		t.Run(tc.given, func(t *testing.T) {
+			id := ID(tc.given)
+			actual := id.Path()
+			if tc.expected != actual {
+				t.Fatalf("expected %v, got %v", tc.expected, actual)
+			}
+		})
 	}
 }
